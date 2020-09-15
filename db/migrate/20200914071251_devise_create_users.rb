da15ab4c42a09be9060de6a5ec901ab1218b16cf
kenta sugimura <sugimura6scjt@yahoo.co.jp>
2020-09-15 12:37:46 +0900

    user

--- db/migrate/20200914071251_devise_create_users.rb
+++ db/migrate/20200914071251_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -35,6 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       t.timestamps null: false
     end
+    add_index :users, :name, unique: true
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
